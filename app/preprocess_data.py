a269f5f7d93f635e00ddca71eabf5ef174d82722
francisco.chacon <chaconrubiof@gmail.com>
2023-07-04 16:38:49 +0200

    added fecha_hora to dataframe

--- app/preprocess_data.py
+++ app/preprocess_data.py
@@ -1,11 +1,16 @@
 from pyspark.sql import SparkSession
 from schemas import traffic_sensor_schema       
+from pyspark.sql.functions import lit
+import time
+
+with open("data/traffic_data.xml", encoding="utf-8") as xml:
+    xml.readline()
+    fecha_hora = xml.readline().split(">",1)[1].split("<")[0]
 
 spark_session = (SparkSession
                     .builder
                     .config("spark.jars.packages", "com.databricks:spark-xml_2.12:0.13.0") \
-                    .getOrCreate()
-                )
+                    .getOrCreate())
 
 logger = spark_session._jvm.org.apache.log4j
 logger.LogManager.getLogger("org").setLevel(logger.Level.FATAL)
@@ -17,4 +22,5 @@ df = spark_session.read \
         .options(rowTag='pm') \
         .load("data/traffic_data.xml", schema=customSchema)
 
+df = df.withColumn("fecha_hora", lit(fecha_hora))
 df.show()
